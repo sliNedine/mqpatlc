58811a8814de85bb661be780392d1fd3ebcf4072
sliNedine <knilsmonster@hotmail.com>
2025-05-24 00:33:14 +0200

    [WIP] create solution and guess + change color of guess

--- src/app/components/master-mind/master-mind.component.ts
+++ src/app/components/master-mind/master-mind.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
 
+export const colorMap: Record<number, string> = {
+  1: 'red',
+  2: 'yellow',
+  3: 'green',
+  4: 'blue',
+  5: 'purple',
+};
 @Component({
   selector: 'app-master-mind',
   imports: [],
   templateUrl: './master-mind.component.html',
-  styleUrl: './master-mind.component.scss'
+  styleUrl: './master-mind.component.scss',
 })
 export class MasterMindComponent {
+  protected solution: number[] = [];
+  protected guess: (number | undefined)[] = [
+    undefined,
+    3,
+    undefined,
+    undefined,
+    undefined,
+  ];
 
+  protected colorMap = colorMap;
+
+  public constructor() {
+    this.solution = this.createSolution();
+  }
+
+  private createSolution(): number[] {
+    const result: number[] = [];
+    for (let i = 0; i < 5; i++) {
+      const randomNum = 1 + Math.floor(Math.random() * 5);
+      result.push(randomNum);
+    }
+    return result;
+  }
+
+  protected changeSingleGuessColor(index: number): void {
+    const value = this.guess[index];
+    if (value === undefined) {
+      this.guess[index] = 1;
+    } else if (value === 5) {
+      this.guess[index] = undefined;
+    } else {
+      this.guess[index] = this.guess[index]! + 1;
+    }
+  }
 }
