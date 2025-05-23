c4d493d2d27e86a84cb705f68281a0136657e9f8
sliNedine <knilsmonster@hotmail.com>
2025-05-23 23:49:46 +0200

    rename color service

---
+++ src/app/services/color.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ColorService {
+  private channelVars = {
+    r: '--actualR',
+    g: '--actualG',
+    b: '--actualB',
+  };
+
+  unlockChannel(channel: 'r' | 'g' | 'b') {
+    document.documentElement.style.setProperty(
+      this.channelVars[channel],
+      '255'
+    );
+  }
+
+  lockChannel(channel: 'r' | 'g' | 'b') {
+    document.documentElement.style.setProperty(this.channelVars[channel], '0');
+  }
+}
